5a7af991476aea37752e304579c2781732f8e0b4
Glayton Martins <gr.martins@outlook.com>
2022-05-01 11:07:08 -0300

    Register Blueprints

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,2 +1,11 @@
+from app.controllers.main import main
+from app.controllers.handlers import errors
+from app.controllers.posts import posts
+from app.controllers.users import users
+
+
 def init_app(app):
-    ...
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+    app.register_blueprint(posts)
+    app.register_blueprint(users)
